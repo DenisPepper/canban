524ee7705c8104b5cf9d1742a224b77a21964d12
DenisPepper <thech@yandex.ru>
2023-04-29 20:57:36 +0300

    Добавит контекст состояния приложения

---
+++ src/state/app-state-context.tsx
@@ -0,0 +1,44 @@
+import {createContext, ReactNode, useContext} from "react";
+
+interface Task {
+    id: string;
+    text: string;
+}
+
+interface Column {
+    id: string;
+    title: string;
+    tasks: Task[];
+}
+
+export interface AppState {
+    columns: Column[];
+}
+
+const appData: AppState = {
+    columns: [
+        {id: '0', tasks: [{id: 'c0', text: 'task one'}], title: 'To do'},
+        {id: '1', tasks: [{id: 'c1', text: 'task two'}], title: 'To do'},
+        {id: '2', tasks: [{id: 'c2', text: 'task three'}], title: 'To do'},
+    ],
+};
+
+interface AppStateContextProps {
+    columns: Column[];
+    getTasksByColumnId(id: string): Task[];
+}
+
+const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps);
+
+export const AppStateProvider = (props: {children: ReactNode}) => {
+    const {children} = props;
+    const {columns} = appData;
+
+    const getTasksByColumnId = (id: string) => columns.find((column) => column.id === id)?.tasks || [];
+
+    return (
+      <AppStateContext.Provider value={{columns, getTasksByColumnId}}>{children}</AppStateContext.Provider>
+    );
+}
+
+export const useAppState = () => useContext(AppStateContext);
